a320c3a2a688ddf2a2fba41a06167bf3a5d0f744
Henrik Norstrom <henrik.norstrom@annotell.com>
2020-12-01 22:46:43 +0100

    solve without loops

--- day_1.py
+++ day_1.py
@@ -1,36 +1,44 @@
 import pandas as pd
 import numpy as np
+import multiprocessing as mp
+import functools
 
 input = 'inputs/day_1.txt'
 
 df = pd.read_csv(input, '\n')
 entries = df.iloc[:,0]
 
-# part one
+def add_to_entries(x):
+    return x+entries
 
-for i in range(0, len(entries)):
-    rolled_entries = np.roll(entries, i)
-    result = np.where(entries + rolled_entries == 2020)
+def add_to_entries2(x):
+    return map(add_to_entries, x)
+
+def search_for_match(x):
+    result = np.where(x == 2020)
     if result[0].size > 0:
         index = result[0][0]
-        answer = rolled_entries[index]*entries[index]
-        print(f'Answer to part one: {answer}')
-        break
+        return index
 
-#part two
+def search_for_match2(x):
+    return list(map(search_for_match, x))
 
-found_solution = False
-for i in range(0, len(entries)):
-    rolled = np.roll(entries, i)
-    for j in range(0, len(entries)):
-        rolled2 = np.roll(entries, j)
-        result = np.where(entries + rolled + rolled2 == 2020)
-        if result[0].size > 0:
-            index = result[0][0]
-            answer = rolled[index]*entries[index]*rolled2[index]
-            found_solution = True
-            print(f'Answer to part two: {answer}')
-            break
-    if found_solution:
-        break
+if __name__ == '__main__':
+    pool = mp.Pool()
 
+    # Part one
+    added_lists = pool.starmap(add_to_entries, zip(entries))
+    indices = np.array(pool.starmap(search_for_match, zip(added_lists)))
+    filtered_indices = (indices[pd.notnull(indices)])
+    filtered_entries = entries[filtered_indices]
+    answer = functools.reduce(lambda x, y: x*y, filtered_entries)
+    print(f'Answer to part one: {answer}')
+
+    # Part two
+    added_lists = pool.starmap(add_to_entries2, zip(added_lists))
+    indices = np.array(pool.starmap(search_for_match2, zip(added_lists)))
+    filtered_indices = indices[pd.notnull(indices)]
+    unique_indices = list(set(filtered_indices))
+    filtered_entries = entries[unique_indices]
+    answer = functools.reduce(lambda x, y: x*y, filtered_entries)
+    print(f'Answer to part two: {answer}')
