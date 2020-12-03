0a731755f5aeac9efadb480a9828b3d48b123104
Henrik Norstrom <henrik.norstrom@annotell.com>
2020-12-03 09:57:23 +0100

    use mapping for finding trees

--- day_3.py
+++ day_3.py
@@ -1,34 +1,42 @@
 import functools
 import numpy as np
+from typing import Tuple
 
-max_horizontal = 30
+with open('inputs/day_3.txt', 'r') as file:
+    lines = file.readlines()
+
+lines = list(map(str.strip, lines))
+horizontal_range = len(lines[0])-1
+vertical_range = len(lines)
 
 
 def horizontal_location(x: int) -> int:
-    if x > max_horizontal:
-        x = x % (max_horizontal + 1)
+    if x > horizontal_range:
+        x = x % (horizontal_range + 1)
     return x
 
 
+def is_tree(vertical_coordinate: int, horizontal_coordinate: int) -> bool:
+    character = lines[vertical_coordinate][horizontal_location(horizontal_coordinate)]
+    return character == '#'
+
+
+def find_trees(slope: Tuple[int, int]) -> int:
+    (right, down) = slope
+
+    vertical_coordinates = np.linspace(0, vertical_range-1, round(vertical_range/down))
+    vertical_coordinates = list(map(int, vertical_coordinates))
+
+    horizotal_coordinates = np.linspace(0, right/down*(vertical_range-1), round(vertical_range/down))
+    horizotal_coordinates = list(map(int, horizotal_coordinates))
+
+    found_trees = list(map(is_tree, vertical_coordinates, horizotal_coordinates))
+    return sum(found_trees)
+
+
 if __name__ == '__main__':
-    with open('inputs/day_3.txt', 'r') as file:
-        lines = file.readlines()
-
-    lines = list(map(str.strip, lines))
-    vertical_range = len(lines)
-    tree_list = []
-    for slope in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
-        (right, down) = slope
-        vertical_coordinates = np.linspace(0, vertical_range-1, round(vertical_range/down))
-        vertical_coordinates = list(map(int, vertical_coordinates))
-        horizotal_coordinates = np.linspace(0, right/down*(vertical_range-1), round(vertical_range/down))
-        horizotal_coordinates = list(map(int, horizotal_coordinates))
-
-        trees = 0
-        for i, vertical_coordinate in enumerate(vertical_coordinates):
-            if lines[vertical_coordinate][horizontal_location(horizotal_coordinates[i])] == '#':
-                trees += 1
-        tree_list.append(trees)
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    tree_list = list(map(find_trees, slopes))
 
     answer = functools.reduce(lambda x, y: x*y, tree_list)
 
