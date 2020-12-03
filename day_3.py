da9a89b7829a9ffde70be19ab8c02016fe669e72
Henrik Norstrom <henrik.norstrom@annotell.com>
2020-12-03 09:39:14 +0100

    add solution for day 3 part 2

--- day_3.py
+++ day_3.py
@@ -1,3 +1,4 @@
+import functools
 import numpy as np
 
 max_horizontal = 30
@@ -14,16 +15,22 @@ if __name__ == '__main__':
         lines = file.readlines()
 
     lines = list(map(str.strip, lines))
-
     vertical_range = len(lines)
-    vertical_coordinates = np.linspace(0, vertical_range-1, vertical_range)
-    vertical_coordinates = list(map(int, vertical_coordinates))
-    horizotal_coordinates = np.linspace(0, 3*(vertical_range-1), vertical_range)
-    horizotal_coordinates = list(map(int, horizotal_coordinates))
+    tree_list = []
+    for slope in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
+        (right, down) = slope
+        vertical_coordinates = np.linspace(0, vertical_range-1, round(vertical_range/down))
+        vertical_coordinates = list(map(int, vertical_coordinates))
+        horizotal_coordinates = np.linspace(0, right/down*(vertical_range-1), round(vertical_range/down))
+        horizotal_coordinates = list(map(int, horizotal_coordinates))
 
-    trees = 0
-    for vertical_coordinate in vertical_coordinates:
-        if lines[vertical_coordinate][horizontal_location(horizotal_coordinates[vertical_coordinate])] == '#':
-            trees += 1
+        trees = 0
+        for i, vertical_coordinate in enumerate(vertical_coordinates):
+            if lines[vertical_coordinate][horizontal_location(horizotal_coordinates[i])] == '#':
+                trees += 1
+        tree_list.append(trees)
 
-    print(trees)
+    answer = functools.reduce(lambda x, y: x*y, tree_list)
+
+    print(f'Answer to part one: {tree_list[1]}')
+    print(f'Answer to part two: {answer}')
