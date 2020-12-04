85e42bb6728596741daf719cc0f08dfef92811b5
Henrik Norstrom <henrik.norstrom@annotell.com>
2020-12-04 09:02:58 +0100

    add part 2

--- day_4.py
+++ day_4.py
@@ -1,17 +1,47 @@
-from typing import List
+import re
 
 mandatory_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-optional_fields = ['cid']
 
 
-def extract_keys(field: str) -> str:
-    key = field.split(':')
-    return key[0]
+def validate_field(key: str, value: str) -> bool:
+    valid = False
 
+    if key == 'byr':  # Birth Year
+        value = int(value)
+        valid = value in range(1920, 2003)
+    elif key == 'iyr':  # Issue Year
+        value = int(value)
+        valid = value in range(2010, 2021)
+    elif key == 'eyr':  # Expiration Year
+        value = int(value)
+        valid = value in range(2020, 2031)
+    elif key == 'hgt':  # Height
+        match = re.match(r"([0-9]+):?(cm|in)", value)
+        if match:
+            matching_characters = match.span()[1]
+            if matching_characters == len(value):
+                (height, unit) = match.groups()
+                if unit == 'cm':
+                    valid = int(height) in range(150, 194)
+                elif unit == 'in':
+                    valid = int(height) in range(59, 77)
+    elif key == 'hcl':  # Hair Color
+        if value[0] == '#' and len(value) == 7:
+            matching_characters = re.match(r"(?:[a-z]|[0-9])+", value[1:7]).span()[1]
+            if matching_characters == 6:
+                valid = True
+    elif key == 'ecl':  # Eye Color
+        valid = value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+    elif key == 'pid':  # Passport ID
+        try:
+            int(value)
+            valid = len(value) == 9
+        except:
+            pass
+    elif key == 'cid':  # Country ID
+        valid = True
 
-def extract_fields(line: str) -> List[str]:
-    fields = line.split(' ')
-    return fields
+    return valid
 
 
 if __name__ == '__main__':
@@ -19,23 +49,36 @@ if __name__ == '__main__':
         lines = file.readlines()
     lines = list(map(str.strip, lines))
 
-    mandatory_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-    optional_fields = ['cid']
-
-    passports = [[]]
+    passports_data = [[]]
     passport_index = 0
     for line in lines:
-        fields = extract_fields(line)
-        keys = list(map(extract_keys, fields))
-        if keys == ['']:
-            passports.append([])
+        if line == '':
+            passports_data.append([])
             passport_index += 1
         else:
-            passports[passport_index] += keys
+            passports_data[passport_index].append(line)
 
+    passports = []
+    for i, passport_data in enumerate(passports_data):
+        passports.append({})
+        for data_line in passport_data:
+            pairs = data_line.split(' ')
+            for pair in pairs:
+                (key, value) = pair.split(':')
+                passports[i][key] = value
+
+    contains_fields_count = 0
     valid_count = 0
     for passport in passports:
-        if set(mandatory_fields).issubset(passport):
-            valid_count += 1
+        fields_valid = True
+        contains_mandatory_fields = set(mandatory_fields).issubset(passport.keys())
+        for (key, value) in passport.items():
+            if not validate_field(key, value):
+                fields_valid = False
 
-    print(valid_count)
+        if contains_mandatory_fields:
+            contains_fields_count += 1
+        if contains_mandatory_fields and fields_valid:
+            valid_count += 1
+    print(f'Part one answer: {contains_fields_count}')
+    print(f'Part two answer: {valid_count}')
