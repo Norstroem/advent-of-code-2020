a259f7505e256db315e40beffea1fabc2b102161
Henrik Norstrom <henrik.norstrom@annotell.com>
2020-12-05 11:10:48 +0100

    add part two

--- day_5.py
+++ day_5.py
@@ -1,12 +1,13 @@
-input_string = 'FBLR'
-translation_string = '0101'
-translation = str.maketrans(input_string, translation_string)
-
 if __name__ == '__main__':
     with open('inputs/day_5.txt', 'r') as file:
         lines = file.readlines()
     boarding_passes = map(str.strip, lines)
+
+    input_string = 'FBLR'
+    translation_string = '0101'
+    translation = str.maketrans(input_string, translation_string)
     boarding_passes_binary = list(map(lambda x: x.translate(translation), boarding_passes))
+
     row_range = 7
     rows_binary = map(lambda x: x[0:row_range], boarding_passes_binary)
     columns_binary = map(lambda x: x[row_range:], boarding_passes_binary)
@@ -14,6 +15,8 @@ if __name__ == '__main__':
     rows = map(lambda x: int(x, 2), rows_binary)
     columns = map(lambda x: int(x, 2), columns_binary)
 
-    seat_ids = map(lambda x, y: 8*x + y, rows, columns)
+    seat_ids = list(map(lambda x, y: 8*x + y, rows, columns))
+    missing_seat_id = list(set(range(min(seat_ids), max(seat_ids) + 1)) - set(seat_ids))[0]
 
     print(f'Answer to part one: {max(seat_ids)}')
+    print(f'Answer to part two: {missing_seat_id}')
