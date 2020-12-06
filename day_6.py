6d37284ad201dfa5a6959bada16fe7d7e7ada0b6
Henrik Norstrom <henrik.norstrom@annotell.com>
2020-12-06 10:57:05 +0100

    add part two

--- day_6.py
+++ day_6.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from typing import List
 
 
 def flatten(str1: str, str2: str) -> str:
@@ -9,6 +10,18 @@ def flatten(str1: str, str2: str) -> str:
     return f'{str1.strip()}{separator}{str2.strip()}'
 
 
+def flatten2(str1: str, str2: str) -> str:
+    if str2.strip() == '':
+        separator = '|'
+    else:
+        separator = ' '
+    return f'{str1.strip()}{separator}{str2.strip()}'
+
+
+def intersection(sets: List[set]):
+    return reduce(lambda x, y: x.intersection(y), sets)
+
+
 if __name__ == '__main__':
     with open('inputs/day_6.txt', 'r') as file:
         lines = file.readlines()
@@ -17,3 +30,11 @@ if __name__ == '__main__':
     answer_sets = map(set, group_answers)
     yes_answers = list(map(len, answer_sets))
     print(f'Answer to part one: {sum(yes_answers)}')
+
+    answers2 = reduce(flatten2, lines)
+    group_answers = answers2.split('|')
+    group_answers = list(map(lambda x: x.strip().split(' '), group_answers))
+    group_answers_sets = list(map(lambda x: list(map(set, x)), group_answers))
+    intersections = list(map(intersection, group_answers_sets))
+    yes_answers = list(map(len, intersections))
+    print(f'Answer to part two: {sum(yes_answers)}')
